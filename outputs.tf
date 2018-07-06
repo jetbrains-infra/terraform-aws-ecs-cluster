ca99ef02fe95957effdece55d4255653179fe93a
Sergey Ugdyzhekov <sergey.ugdyzhekov@jetbrains.com>
2018-07-06 17:19:59 +0300

    Add cluster_id and service_role name to output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "name" {
   value = "${aws_ecs_cluster.default.name}"
 }
+
+output "ecs_service_role_name" {
+  value = "${aws_iam_role.ecs_service_role.name}"
+}
+
+output "cluster_id" {
+  value = "${aws_ecs_cluster.default.id}"
+}
