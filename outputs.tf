d7c7d3ba6a2017116461312b5e7f1eedc3803c21
Sergei Ugdyzhekov <sergey.ugdyzhekov@jetbrains.com>
2020-03-23 17:43:34 +0100

    Add security group name to output

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,31 @@
 output "name" {
-  value = "${aws_ecs_cluster.default.name}"
+  value = aws_ecs_cluster.default.name
 }
 
 output "id" {
-  value = "${aws_ecs_cluster.default.id}"
+  value = aws_ecs_cluster.default.id
 }
 
 output "ecs_service_role_name" {
-  value = "${aws_iam_role.ecs_service_role.name}"
+  value = aws_iam_role.ecs_service_role.name
 }
 
 output "ecs_default_task_role_name" {
-  value = "${aws_iam_role.ecs_task_role.name}"
+  value = aws_iam_role.ecs_task_role.name
 }
 
 output "iam_instance_profile_arn" {
-  value = "${aws_iam_instance_profile.ecs_node.arn}"
+  value = aws_iam_instance_profile.ecs_node.arn
 }
 
 output "iam_instance_profile_name" {
-  value = "${aws_iam_instance_profile.ecs_node.name}"
+  value = aws_iam_instance_profile.ecs_node.name
 }
 
 output "security_group_id" {
-  value = "${aws_security_group.ecs_nodes.id}"
+  value = aws_security_group.ecs_nodes.id
+}
+
+output "security_group_name" {
+  value = aws_security_group.ecs_nodes.name
 }
