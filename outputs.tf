73f001801e9a14be8418d8cc3964d5f38ebbd306
Sergei Ugdyzhekov <sergey.ugdyzhekov@jetbrains.com>
2020-06-28 20:17:37 +0200

    Add service role arn and task role arn to output

--- outputs.tf
+++ outputs.tf
@@ -10,8 +10,16 @@ output "ecs_service_role_name" {
   value = aws_iam_role.ecs_service_role.name
 }
 
+output "ecs_service_role_arn" {
+  value = aws_iam_role.ecs_service_role.arn
+}
+
 output "ecs_default_task_role_name" {
   value = aws_iam_role.ecs_task_role.name
+}
+
+output "ecs_default_task_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
 }
 
 output "iam_instance_profile_arn" {
