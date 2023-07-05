46e41a55817981118161636e7a28e9ada4dc5a5d
Andrey Mulin <andrey.mulin@jetbrains.com>
2023-07-05 11:34:33 +0300

    Revert "Remove default_capacity_provider_strategy because it prevents to start Service with scheduling_strategy = "DAEMON""
    This reverts commit 086c33e0d8b9dc39f25bc75252393887a3cb2281.

--- ecs.tf
+++ ecs.tf
@@ -7,6 +7,12 @@ resource "aws_ecs_cluster_capacity_providers" "example" {
   cluster_name = aws_ecs_cluster.default.name
 
   capacity_providers = [aws_ecs_capacity_provider.asg.name]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = aws_ecs_capacity_provider.asg.name
+  }
 }
 
 resource "aws_ecs_capacity_provider" "asg" {
